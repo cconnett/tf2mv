49b2574331a5b2b23f919139184a51824b58938d
Chris Connett <chris@connett.net>
2011-04-22 15:00:43 -0400

    Improve error handling in itemFound.

--- website/itemFound.py
+++ website/itemFound.py
@@ -14,12 +14,17 @@ for i in range(5):
 def itemFound():
     try:
         mysql = mysqls.get(block=False)
+    except Queue.Empty:
+        app.logger.error('Not enough mysql connections.')
+        return
+    try:
         cursor = mysql.cursor()
         cursor.execute('insert into item_found values (INET_ATON(?), NULL, ?, ?, ?, ?, ?)',
                        (request.ip, request.form['steamid'], request.form['method'],
                         request.form['quality'], request.form['item'], request.form['propername']))
+        cursor.close()
         mysql.commit()
         mysqls.put(mysql)
-    except:
-        pass
-    return ''
+    except Exception as e:
+        app.logger.error(e)
+    return
