a8520c5ed49ef31a226075b24ad11923c95d8b03
Chris Connett <chris@connett.net>
2011-04-22 15:07:30 -0400

    Send logged errors via email.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,16 @@
-import sys, os
+import sys
+import os
+import logging
+from logging import SMTPHandler
+
 INTERP = os.path.join(os.environ['HOME'], 'flask_env', 'bin', 'python')
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 sys.path.append(os.getcwd())
 from website import app as application
+
+mail_handler = SMTPHandler('mail.tf2mv.com', 'no-reply@tf2mv.com',
+                           'errors@tf2mv.com', 'tf2mv Error Logged')
+
+mail_handler.setLevel(logging.ERROR)
+app.logger.addHandler(mail_handler)
