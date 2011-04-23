e95163a627dcc2247629ca7d23a118c1f7d5e2e1
Chris Connett <chris@connett.net>
2011-04-22 23:03:42 -0400

    Fix errors in itemFound method.

--- website/itemFound.py
+++ website/itemFound.py
@@ -1,14 +1,17 @@
 from website import app
+from flask import request
 
 import MySQLdb
 import Queue
 
 # Super simple connection pooling
 mysqls = Queue.Queue()
-for i in range(5):
+for i in range(1):
     try:
-        mysqls.put(MySQLdb.connect(user='item_found', passwd='redbull', db='tf2mv'))
-    except: pass
+        mysqls.put(MySQLdb.connect(host='mysql.tf2mv.com', user='item_found', passwd='redbull', db='tf2mv'))
+    except:
+        pass
+        raise
 
 @app.route('/itemFound', methods=['POST'])
 def itemFound():
@@ -16,15 +19,16 @@ def itemFound():
         mysql = mysqls.get(block=False)
     except Queue.Empty:
         app.logger.error('Not enough mysql connections.')
-        return
+        return ''
     try:
         cursor = mysql.cursor()
-        cursor.execute('insert into item_found values (INET_ATON(?), NULL, ?, ?, ?, ?, ?)',
-                       (request.ip, request.form['steamid'], request.form['method'],
-                        request.form['quality'], request.form['item'], request.form['propername']))
-        cursor.close()
+        cursor.execute('insert into item_found values (NULL, INET_ATON(%s), NULL, %s, %s, %s, %s, %s)',
+                       (request.remote_addr, request.form['steamid'], request.form['method'],
+                        request.form['quality'], request.form['item'], request.form['propername'] == '1'))
         mysql.commit()
-        mysqls.put(mysql)
     except Exception as e:
         app.logger.error(e)
-    return
+    finally:
+        cursor.close()
+        mysqls.put(mysql)
+    return ''
