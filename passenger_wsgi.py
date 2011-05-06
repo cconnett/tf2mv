33bdf6e05193e545dc0cda7164711bf1c6f92fb6
Chris Connett <chris@connett.net>
2011-05-06 16:07:37 -0400

    Use authenticated SMTP to send error mails.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -10,9 +10,17 @@ sys.path.append(os.getcwd())
 from website import application, config
 
 import logging
-from logging.handlers import SMTPHandler
+from handlers import SMTPHandler
+# This handlers comes from ./handlers.py, which is a checkout of
+# Python 2.7's handlers.  This is needed for authenticated SMTP on
+# Python 2.5.
 
+credentials = None
+if 'credentials' in config['mail']:
+    credentials = (config['mail']['credentials']['username'],
+                   config['mail']['credentials']['password'])
 mail_handler = SMTPHandler(config['mail']['smtp'], config['mail']['from'],
-                           config['mail']['errors'], config['mail']['errorsubject'])
+                           config['mail']['errors'], config['mail']['errorsubject'],
+                           credentials)
 mail_handler.setLevel(logging.ERROR)
 application.logger.addHandler(mail_handler)
